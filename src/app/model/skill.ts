1ec5700035796f42b0744b762fccfceb604efcba
artucorreia <arthurcorria0109@gmail.com>
2024-11-14 10:13:52 -0300

    feat (about): refactor about component

--- src/app/model/skill.ts
+++ src/app/model/skill.ts
@@ -1,5 +1,4 @@
 export interface Skill {
-    id: number;
     name: string;
     imageUrl: string;
 }
--- src/app/modules/home/components/about/about.component.ts
+++ src/app/modules/home/components/about/about.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Skill } from '../../../../model/skill';
-import { SkillService } from '../../../../services/skill/skill.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -13,12 +12,50 @@ import { CommonModule } from '@angular/common';
 export class AboutComponent {
   public skills: Skill[] = []
   
-  constructor(private service: SkillService) {}
+  constructor() {}
 
   ngOnInit() {
-    this.service.findAll().subscribe({
-      next: skills => this.skills = skills,
-      error: error => console.error(error)
-    });
+    this.skills = [
+      {
+        name: 'Spring Boot',
+        imageUrl: 'assets/png/spring-boot.png'
+      },
+      {
+        name: 'Java',
+        imageUrl: 'assets/png/java.png'
+      },
+      {
+        name: 'JWT',
+        imageUrl: 'assets/png/jwt.png'
+      },
+      {
+        name: 'Angular',
+        imageUrl: 'assets/png/angular.png'
+      },
+      {
+        name: 'TypeScript',
+        imageUrl: 'assets/png/typescript.png'
+      },
+      {
+        name: 'Scss',
+        imageUrl: 'assets/png/sass.png'
+      },
+      {
+        name: 'MySQL',
+        imageUrl: 'assets/png/mysql.png'
+      },
+      {
+        name: 'PostgreSQL',
+        imageUrl: 'assets/png/postgresql.png'
+      },
+      {
+        name: 'Git',
+        imageUrl: 'assets/png/git.png'
+      },
+      {
+        name: 'Docker',
+        imageUrl: 'assets/png/docker.png'
+      }
+    ]
   }
 }
